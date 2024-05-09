607e131924e39aeac95a6b8ac50943c8480024b8
Liam Simmons <liam.simmons@powerhrg.com>
2024-05-09 15:48:59 -0400

    Add functions to search driver array

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+
+
+const findMatching = (array, string) => {
+  const stringCap = string.toUpperCase()
+  return array.filter((item) => item.toUpperCase() === stringCap);
+
+}
+
+const fuzzyMatch = (array, string) => {
+  const firstLetters = string.toUpperCase()
+  return array.filter((item) => item.toUpperCase().startsWith(firstLetters))
+}
+
+const matchName = (array, string) => {
+  const driverName = string.toUpperCase()
+  return array.filter(item => item.name.toUpperCase() === driverName)
+}
+
+console.log(findMatching(drivers, 'Sarah'))
+console.log(fuzzyMatch(drivers, 'Bob'))
+
